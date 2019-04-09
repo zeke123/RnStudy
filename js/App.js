83ad02aa95983753f94461ae793f7527438c1499
周建(88405798) <88405798@suning.com>
2019-04-09 17:15:08 +0800

    Hello React Native

--- js/App.js
+++ js/App.js
@@ -11,7 +11,6 @@ import React, {Component} from 'react';
 
 import {StyleSheet, Text, View} from 'react-native';
 
-
 type Props = {};
 export default class App extends Component<Props> {
     render() {
